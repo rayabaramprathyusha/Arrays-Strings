70707f58f5abf24c48b6e9e2b152321fcaaefb77
rayabaramprathyusha <rayabaramprathyusha2810@gmail.com>
2025-10-03 10:21:34 -0500

    Update StockPriceAnalyzer.java

--- StockPriceAnalyzer.java
+++ StockPriceAnalyzer.java
@@ -1,27 +1,28 @@
 import java.util.Scanner;
 
-public class StockPriceAnalyzer {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+public class StockPriceAnalyzer{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
 
         System.out.print("Enter number of days: ");
-        int n = sc.nextInt();
-        int[] prices = new int[n];
+        int n=sc.nextInt();
+        int[] prices=new int[n];
 
-        System.out.println("Enter stock prices:");
-        for (int i = 0; i < n; i++) {
-            prices[i] = sc.nextInt();
+
+        System.out.print("Enter stock prices: ");
+        for(int i=0;i<n;i++){
+            prices[i]=sc.nextInt();
+
         }
-
-        int max = prices[0], min = prices[0];
-        for (int price : prices) {
-            if (price > max) max = price;
-            if (price < min) min = price;
+        int max=prices[0],min=prices[0];
+        for(int price:prices){
+            if(price>max) max=price;
+            if(price<min) min=price;
         }
-
-        System.out.println("Highest Price: " + max);
-        System.out.println("Lowest Price: " + min);
+        System.out.println("Maximum stock price is "+max);
+        System.out.println("Minimum stock price is "+min);
 
         sc.close();
+
     }
 }
