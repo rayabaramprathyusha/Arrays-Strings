ba69c53cdfee2c9f32b645967d37465594a42e22
rayabaramprathyusha <rayabaramprathyusha2810@gmail.com>
2025-10-03 09:38:55 -0500

    Add files via upload

---
+++ StockPriceAnalyzer.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class StockPriceAnalyzer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of days: ");
+        int n = sc.nextInt();
+        int[] prices = new int[n];
+
+        System.out.println("Enter stock prices:");
+        for (int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+
+        int max = prices[0], min = prices[0];
+        for (int price : prices) {
+            if (price > max) max = price;
+            if (price < min) min = price;
+        }
+
+        System.out.println("Highest Price: " + max);
+        System.out.println("Lowest Price: " + min);
+
+        sc.close();
+    }
+}
